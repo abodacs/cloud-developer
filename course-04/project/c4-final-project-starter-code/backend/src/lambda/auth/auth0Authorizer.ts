26f672ccc82b2782e3367c784ffc0e9d74e93c6c
abdullah <abdullahabdelrhim@gmail.com>
2020-02-08 14:12:33 +0200

    conect to auth0

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -12,10 +12,26 @@ const logger = createLogger('auth')
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
 
-const jwksUrl = process.env.JWT_URL
-const cert = await Axios.get(jwksUrl).promise()
+const cert = `-----BEGIN CERTIFICATE-----
+MIIDDTCCAfWgAwIBAgIJQoL0tv6hGdF7MA0GCSqGSIb3DQEBCwUAMCQxIjAgBgNV
+BAMTGWRldi1ncjZzeWR2bC5ldS5hdXRoMC5jb20wHhcNMjAwMjA4MDk1OTI1WhcN
+MzMxMDE3MDk1OTI1WjAkMSIwIAYDVQQDExlkZXYtZ3I2c3lkdmwuZXUuYXV0aDAu
+Y29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwkOhBuyL7B5zoHVu
+CVSo93gEgBSiNlws+J21PGARBvUU9YOjV7OMrOSx9LbbgTHN9koLWINRizJ6TS23
+x6NQtRa8nzAXXr3l7WR7bFLg50qzDWRnIMZk8GJ7364i0AwfMtJZ7QE4n0sKtpbu
+sMHxeiPi1BVr2QxR2I6r8IgPHn4qhHACeMWgHO5Q0rZdzZ1jAi+2Uvw4Fi8Bt/D1
+3NGKHFtLLHYCkuCEIjIHQPcOLyKUsMcY5l94yoqHibwHWBsantW0igQz4TWEV/2o
+V9fpUozrqISjtF1Jb0FYHgMIq0vX0SSnt4ryOiqe1Z6CV85bdIbGnfubk6jcBCHa
+MDsBVwIDAQABo0IwQDAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBQXaUvujCge
+TW0R3OoXDID2dE8jZjAOBgNVHQ8BAf8EBAMCAoQwDQYJKoZIhvcNAQELBQADggEB
+AKi4oB8zwzLE77TgOx8NcLw0/GJrqJRq+clAQCXVoJhBz0eM4OfrZNWTr+6Fhh/Q
+MWkNnLPj0XV6TDhKtKPRwvEi97WwlFlpydyrjW9RZnkFicgS94wk04dGZhx/PxpU
+EyC4RzNo12vcz0ZQgh7HNIOhTC7aSx7BLFfuZgN7CX0quZVwG02ru1Fj5lw1TrUv
+2vMfQak96Wz+UDdh+L8OgtKugMDK2Eu4bJJL1Ncg7CogEUMIC5qGAbAh3FdVY/sW
+UuGhqPltx2idssDO6D93vRwSHJhI0SNj8llKXK9OXPghyesvX43b/g4NX8IonTfa
+ShQNF2C659Bz+FIQJr/0KT8=
+-----END CERTIFICATE-----`
 
 export const handler = async (
   event: CustomAuthorizerEvent
